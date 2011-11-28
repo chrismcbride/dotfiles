e3381b4987854dea17e3d89c4950068d438c2494
Chris McBride <cmcbride@rjmetrics.com>
2011-11-27 20:07:43 -0500

    added windownavigation

--- xmonad.hs
+++ xmonad.hs
@@ -14,6 +14,7 @@ import XMonad.Layout.MouseResizableTile
 import XMonad.StackSet as W (shift, greedyView)
 import XMonad.Actions.SpawnOn (spawnOn)
 import XMonad.Layout.Tabbed
+import XMonad.Layout.WindowNavigation
 import System.IO
 
 
@@ -23,38 +24,54 @@ main = do
 
 myConfig xmproc = defaultConfig {
 			manageHook = manageDocks <+> ( myManageHooks <+> manageHook defaultConfig ),
-			layoutHook = smartBorders( avoidStruts (myLayout)),
+			layoutHook = windowNavigation $ smartBorders $ avoidStruts $ myLayout,
 			workspaces = myWorkspaces,
 			logHook = myLoghook xmproc,
 			modMask = mod4Mask,	 -- Rebind Mod to the Windows key
 			startupHook = myStartuphook
 		}  
 
-addKeyBindings config = config `additionalKeys` (
-			[ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
-			, ((mod4Mask, xK_p), spawn "dmenu_run")
-			, ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-			, ((mod4Mask .|. shiftMask, xK_F12), spawn "x-terminal-emulator -e 'gdm-control --shutdown && xmonad --restart'")
-			, ((mod4Mask .|. shiftMask, xK_F11), spawn "x-terminal-emulator -e 'gdm-control --restart && xmonad --restart'")
-			, ((mod4Mask .|. shiftMask, xK_Return), spawn "terminator")
-			, ((mod4Mask .|. shiftMask, xK_r), renameWorkspace defaultXPConfig)
-			--XF86AudioMute
-			, ((0,  0x1008ff12), spawn "amixer sset Master toggle")
-			-- XF86AudioLowerVolume
-			, ((0 , 0x1008ff11), spawn "amixer -q sset Master 1- unmute")
-			-- XF86AudioRaiseVolume
-			, ((0 , 0x1008ff13), spawn "amixer -q set Master 1+ unmute")
-			, ((mod4Mask, xK_u), sendMessage ShrinkSlave) -- %! Shrink a slave area
-			, ((mod4Mask, xK_i), sendMessage ExpandSlave) -- %! Expand a slave area
-			, ((0, xK_Print), spawn "scrot")
-			]
-			++
-			[((m .|. mod4Mask, k), windows $ f i)                                                                           
-				| (i, k) <- zip (drop 9 myWorkspaces) [xK_0, xK_minus, xK_equal]
-				, (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-		)
-		`removeKeys`
-		[ (mod4Mask, xK_q) ]
+addKeyBindings config = 
+			config `removeKeys`	
+				[ (mod4Mask, xK_q)
+				, (mod4Mask, xK_j)
+				, (mod4Mask, xK_k)
+				, (mod4Mask, xK_h)
+				, (mod4Mask, xK_l)
+				]
+			`additionalKeys` (
+				[ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+				, ((mod4Mask, xK_p), spawn "dmenu_run")
+				, ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+				, ((mod4Mask .|. shiftMask, xK_F12), spawn "x-terminal-emulator -e 'gdm-control --shutdown && xmonad --restart'")
+				, ((mod4Mask .|. shiftMask, xK_F11), spawn "x-terminal-emulator -e 'gdm-control --restart && xmonad --restart'")
+				, ((mod4Mask .|. shiftMask, xK_Return), spawn "terminator")
+				, ((mod4Mask .|. shiftMask, xK_r), renameWorkspace defaultXPConfig)
+				--XF86AudioMute
+				, ((0,  0x1008ff12), spawn "amixer sset Master toggle")
+				-- XF86AudioLowerVolume
+				, ((0 , 0x1008ff11), spawn "amixer -q sset Master 1- unmute")
+				-- XF86AudioRaiseVolume
+				, ((0 , 0x1008ff13), spawn "amixer -q set Master 1+ unmute")
+				, ((mod4Mask, xK_i), sendMessage ShrinkSlave) -- %! Shrink a slave area
+				, ((mod4Mask, xK_u), sendMessage ExpandSlave) -- %! Expand a slave area
+				, ((mod4Mask, xK_y), sendMessage Shrink) 
+				, ((mod4Mask, xK_o), sendMessage Expand) 
+				, ((mod4Mask,                 xK_l), sendMessage $ Go R)
+				, ((mod4Mask,                 xK_h), sendMessage $ Go L)
+				, ((mod4Mask,                 xK_k), sendMessage $ Go U)
+				, ((mod4Mask,                 xK_j), sendMessage $ Go D)
+				, ((mod4Mask .|. shiftMask, xK_l), sendMessage $ Swap R)
+				, ((mod4Mask .|. shiftMask, xK_h), sendMessage $ Swap L)
+				, ((mod4Mask .|. shiftMask, xK_k), sendMessage $ Swap U)
+				, ((mod4Mask .|. shiftMask, xK_j), sendMessage $ Swap D)
+				, ((0, xK_Print), spawn "scrot")
+				]
+				++
+				[((m .|. mod4Mask, k), windows $ f i)                                                                           
+					| (i, k) <- zip (drop 9 myWorkspaces) [xK_0, xK_minus, xK_equal]
+					, (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+			)
 
 myStartuphook =  do
 	setWMName "LG3D" --java hack
@@ -92,10 +109,10 @@ myManageHooks = composeAll . concat $
 
 myLayout = mouseResizableTile {
 				masterFrac  = 0.6,
-				draggerType = FixedDragger 0 3
+				draggerType = FixedDragger 0 5
 			} 
 			|||	mouseResizableTile {
 				masterFrac = 0.6,
-				draggerType = FixedDragger 0 3,
+				draggerType = FixedDragger 0 5,
 				isMirrored = True
 			} ||| simpleTabbed
